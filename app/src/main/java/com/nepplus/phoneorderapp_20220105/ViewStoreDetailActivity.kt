6a172ed0aac94ecea435dbb73b244098b3924ef3
김현희 <tta_bi@naver.com>
2022-01-05 16:59:42 +0900

    putExtra로 클래스 통째로 첨부 - getSerializableExtra as 데이터 클래스

--- app/src/main/java/com/nepplus/phoneorderapp_20220105/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/nepplus/phoneorderapp_20220105/ViewStoreDetailActivity.kt
@@ -1,11 +1,19 @@
 package com.nepplus.phoneorderapp_20220105
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.phoneorderapp_20220105.models.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+
+        val storeData = intent.getSerializableExtra("store") as StoreData
+
+        txtStoreName.text = storeData.name
+
     }
 }
--- app/src/main/java/com/nepplus/phoneorderapp_20220105/models/StoreData.kt
+++ app/src/main/java/com/nepplus/phoneorderapp_20220105/models/StoreData.kt
@@ -1,8 +1,10 @@
 package com.nepplus.phoneorderapp_20220105.models
 
+import java.io.Serializable
+
 class StoreData(
     val name : String,
     val phoneNum : String,
     val logoURL : String
-) {
+) : Serializable {
 }
